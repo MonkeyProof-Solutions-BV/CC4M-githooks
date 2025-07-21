75c1eb0257cb6953cdb024cd5c2c54cd96088039
Co Melissant <co.melissant@monkeyproofsolutions.nl>
2025-07-21 11:06:28 +0200

    first versions

---
+++ precommit_example.m
@@ -0,0 +1,57 @@
+function precommit_example(filestring, configFile, severityBoundary, doOpenReport, isVerbose)
+    % PRECOMMIT_EXAMPLE The MATLAB side of the GIT pre-commit hook example
+    %
+    % Inputs - required:
+    %  
+    % * filestring      (char)      Comma-separated list of all the files to be checked.
+    %
+    % Inputs - optional
+    % * configFile      (char)      Name/filename of the configuration file.
+
+    % Copyright 2025 Monkeyproof Solutions BV
+
+    arguments
+        filestring                  char
+        configFile                  char        ='MonkeyProofMATLABCodingStandard'
+        severityBoundary    (1,1)   double      =3
+        doOpenReport        (1,1)   logical     =true
+        isVerbose           (1,1)   logical     =true
+    end
+
+    files = strsplit(filestring, ',');
+
+    [cc4mReportUrl, cc4mSummary] = monkeyproof.cc4m.start(...
+        'file',             files, ...
+        'configFile',       configFile, ...
+        'runSeverities',    severityBoundary);
+
+    %% When to fail
+    % HERE define when to fail for this repository
+
+    failCondition = cc4mSummary.Results.NrViolations > 0;               % violations found
+    
+    if isVerbose
+        disp(cc4mReportUrl)
+        disp(cc4mSummary.Results)
+    end
+
+    if failCondition
+
+        if doOpenReport
+            % Make sure files analysed are on the path in order to make the links from the report work.
+
+            folders = {}; % cell array with project path
+
+            % Command to adapt the path.
+            addpathCmd = ['addpath(''', strjoin(folders, ''', '''), ''')'];
+
+            % Start new matlab session to open the report - a new session is 
+            % used so that commit action can be finished immediately.
+            system(['matlab -r ',  addpathCmd ',web(''' cc4mReportUrl ''') &']);
+        end
+
+        exit(1)
+    else
+        exit(0)
+    end
+end
