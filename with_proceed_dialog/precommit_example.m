f69fc49243c9f1e6797cd968aeb7d98fd6987d18
Co Melissant <co.melissant@monkeyproofsolutions.nl>
2025-07-23 16:07:07 +0200

    #1 - added files with a dialog in MATLAB

--- with_proceed_dialog/precommit_example.m
+++ with_proceed_dialog/precommit_example.m
@@ -1,17 +1,17 @@
 function precommit_example(filestring, configFile, severityBoundary, doOpenReport, isVerbose)
     % PRECOMMIT_EXAMPLE The MATLAB side of the GIT pre-commit hook example
     %
-	% Requires CC4M >= v2.18.2
-	%
+    % Requires CC4M >= v2.18.2
+    %
     % Inputs - required:
     %  
     % * filestring       (char)      Comma-separated list of all the files to be checked.
     %
     % Inputs - optional
     % * configFile       (char)      (File)name of CC4M configuration (default: 'MonkeyProofMATLABCodingStandard')
-	% * severityBoundary (double)    Lowest severity that blocks a commit (default:3)	
-	% * doOpenReport     (boolean)   If true (default), opens a the HTML report of the detected violations.
-	% * isVerbose        (boolean)   If true (default), shows some more information in the shell.
+    % * severityBoundary (double)    Lowest severity that blocks a commit (default:3)   
+    % * doOpenReport     (boolean)   If true (default), opens a the HTML report of the detected violations.
+    % * isVerbose        (boolean)   If true (default), shows some more information in the shell.
 
     % Copyright 2025 Monkeyproof Solutions BV
 
@@ -23,7 +23,7 @@ function precommit_example(filestring, configFile, severityBoundary, doOpenRepor
         isVerbose           (1,1)   logical     = true
     end
 
-	clc
+    clc
     files = strsplit(filestring, ',');
 
     [cc4mReportUrl, cc4mSummary] = monkeyproof.cc4m.start(...
@@ -55,19 +55,19 @@ function precommit_example(filestring, configFile, severityBoundary, doOpenRepor
             % used so that commit action can be finished immediately.
             system(['matlab -r ',  addpathCmd ',web(''' cc4mReportUrl ''') &']);
         end
-		
-		% 
-		drawnow()
-		answer = questdlg('Proceed with commit?','Guideline violations found');
+
+        % 
+        drawnow()
+        answer = questdlg('Proceed with commit?','Guideline violations found');
         switch answer
-		
-			case 'Yes'
-			    disp('Altough violations are found, files are committed.')
-				exit(0)
-			otherwise
-				exit(1)
-		end
-				  
+        
+            case 'Yes'
+                disp('Altough violations are found, files are committed.')
+                exit(0)
+            otherwise
+                exit(1)
+        end
+                  
     else
         exit(0)
     end
