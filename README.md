ae5bf48cf642f33e0b3308ba37a895007e0bb8ee
Co Melissant <co.melissant@monkeyproofsolutions.nl>
2026-01-15 14:27:48 +0100

    reorg

--- README.md
+++ README.md
@@ -2,16 +2,34 @@
 
 To maintain high code quality in your repository, it's helpful to run a static code checker before committing changes. In this repository, files are stored for automating a CC4M run on your MATLAB code using a pre-commit hook.
 
-## Block the commit
-The root folder of the repositry contains the files as explained in a [blog](https://monkeyproofsolutions.nl/about/blog/cc4m/using-githooks) on this topic.
+We'll integrate CC4M (Code Checker for MATLAB) into a Git pre-commit hook to automatically verify code compliance against the guidelines before allowing it to be committed. Here’s how it works:
 
-* [pre-commit](pre-commit) shell script executed by Git.
-* [precommit_example.m](precommit_example.m) MATLAB function as called from the shell script.
+* When you commit changes, the pre-commit hook first runs CC4M.
+* If no issues are found, the commit proceeds.
+* If violations are detected, the report is shown and using a dialog the commit can either be canceled, or allowed anyway.
 
-## Block, but allow to overrule
-The folder [with_proceed_dialog](with_proceed_dialog) contains derivatives from the files above, that implement a question dialog in the MATLAB script to allow for committing the changes, regardless of the violations.
+
+## Prerequisits
+In order to be able to use this pre-commit hook, your system needs:
+* MATLAB
+* CC4M
+* Python (supported by used MATLAB installation, see [link](https://nl.mathworks.com/support/requirements/python-compatibility.html).
+* GIT
+
+## Installation
+
+By default, the pre-commit hook runs CC4M in a separate MATLAB session, which is kept alive. Every time files need to be checked, via Python and the matlabengine module, a CC4M run is executed in this MATLAB session. To be able to do so, a Python environment is created with the [matlabengine](https://pypi.org/project/matlabengine/) installed. As the matlabengine is specific to the MATLAB release - and the code needs to be available from the pre-commit hook, an install procedure is available:
+
+1. Clone this repository
+2. Open MATLAB with CC4M installed
+3. Browse in the local working copy to the folder [matlab](./matlab)
+4. Run install.m
+    - Creates a Python environment in `fullfile(userpath(), 'cc4m', 'python')`
+    - Installs the matlabengine package from PyPi
+5. Copy the file `pre-commit` to the folder _'./.git/hooks'_ for all the local repositories you like to equip with the pre-commit action.
 
 
 # Links
 
+* Repository on GitHug:  [https://github.com/MonkeyProof-Solutions-BV/CC4M-githooks](https://github.com/MonkeyProof-Solutions-BV/CC4M-githooks)
 * Details explanation in [blog](https://monkeyproofsolutions.nl/about/blog/cc4m/using-githooks)
---
+++ matlab/install.m
@@ -0,0 +1,81 @@
+function [hasFailed, msg] = install(usePPI)
+    % INSTALL hooks support for GIT
+    %
+
+    % Copyright 2026 MonkeyProof Solutions BV
+
+    arguments (Input)
+        % usePPI (true/false) When true (default), matlabengine is installed from PyPi, otherwise installed from MATLAB
+        % installation.
+        usePPI logical = true
+    end
+
+    arguments (Output)
+        hasFailed
+        msg
+    end
+
+    % Define locations
+    [~, versionInfo] = monkeyproof.cc4m.utils.getEnvironmentInfo();
+    targetDir        = fullfile(userpath, 'cc4m', 'python', versionInfo.MATLABVersionNr);
+    activateCall     = fullfile(targetDir, 'Scripts', 'activate');
+    [~, ~]           = mkdir(targetDir);
+    pythonDir        = fullfile(pwd, '..', 'cc4mpy');
+
+    % Make sure Python environment available.
+    disp("Checking if Python is available.")
+    pe = pyenv();
+    if isempty(pe.Version)
+        % No Python available
+        hasFailed = true;
+        disp("No Python is available.")
+        msg = "No Python Installed or available via MATLAB.";
+    else
+        % Python found
+        pyCmd = pe.Executable;
+
+        % Test if environment already exists
+        pipCommand = "" + activateCall  + " && " + "pip list";
+        [hasFailed, msg] = system(pipCommand);
+
+        if hasFailed
+            % No environment yet.
+            disp("Installing Python environment.")
+
+            % Create venv
+            pipCommand = "cd """ + targetDir  + """ && " + pyCmd + " -m venv """ + targetDir + """ && " + activateCall;
+
+            % Install the engine
+            if usePPI
+                % From Python Package Index
+                pipCommand = pipCommand + ...
+                    " && " + "pip install matlabengine>=" + versionInfo.MATLABVersionNr;
+            else
+                % From local files
+                pipCommand = pipCommand + ...
+                    " && " + "cd """ + fullfile(matlabroot, 'extern', 'engines', 'python') + """" + ...
+                    " && " + "python setup.py build --build-base=""" + targetDir + """"; % --build-base=""" + targetDir + """
+            end
+            [hasFailed, msg] = system(pipCommand);
+        end
+
+    end
+
+    % Install local files
+    if ~hasFailed
+        disp("MATLAB engine installed. Now adding CC4M integration.")
+
+        pipCommand = activateCall  + " && " + "pip install " +pythonDir;
+        [hasFailed, msg] = system(pipCommand);
+
+        if hasFailed
+            disp("CC4M integration could not be installed!")
+            disp(msg)
+        else
+            disp("CC4M integration installed.")
+
+            pipCommand = "" + activateCall  + " && " + "pip list";
+            [hasFailed, msg] = system(pipCommand)
+        end
+    end
+end
